82d1ef7f7c22dbd659c6c86fc9856dcf13da88b9
dabuzida <dabuzida@gmail.com>
2022-03-14 17:55:42 +0900

    flutter 공식문서 introduction to widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,156 +1,53 @@
+// sampels & tutorials > codelabs > Designing a Flutter UI > Basic Flutter layout concepts
+// main_docs_layout2
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
-        ],
-      ),
-    );
-    
-
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'CALL'),
-        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-        _buildButtonColumn(color, Icons.share, 'SHARE'),
-      ],
-    );
-
-    Widget textSection = const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-
     return MaterialApp(
-        // debugShowCheckedModeBanner: false,
-        title: 'Flutter layout demo',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter layout demo'),
-            backgroundColor: Colors.purple[600],
-          ),
-          body: ListView(
-            children: [
-              Image.asset(
-                'images/lake.jpg',
-                width: 600,
-                height: 240,
-                fit: BoxFit.cover,
-              ),
-              titleSection, 
-              buttonSection, 
-              textSection],
-          ),
-        ));
-  }
+        home:
 
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
+            // Container(
+            //     decoration: BoxDecoration(
+            //       border: Border.all(
+            //         color: Colors.lightGreen,
+            //         width: 8,
+            //       ),
+            //       borderRadius: BorderRadius.circular(12),
+            //     ),
+            //     child:
+
+            Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
+        BlueBox(),
+        BlueBox(),
+        Expanded(
+          child: BlueBox(),
         ),
+        BlueBox(),
       ],
-    );
-  }
+    ));
 
-  
+    // ));
+  }
 }
 
-
-
-
-
-
-
-
-
-
-/* class MainPage extends StatelessWidget {
-  const MainPage({Key? key}) : super(key: key);
+class BlueBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Main Page'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-        elevation: 0,
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(),
       ),
-      body: Center(
-          //  Padding(
-          //     padding: EdgeInsets.fromLTRB(50, 100, 0, 0),
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text("C"),
-          Text("PYTHON")
-        ],
-      )),
     );
   }
-} */
+}
