13d29ac90ed72c11722aeaf097b0a0ee2dd580cf
dabuzida <dabuzida@gmail.com>
2022-04-05 22:59:13 +0900

    test container, constraints

--- lib/main.dart
+++ lib/main.dart
@@ -28,110 +28,113 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Constraint Sample',
         // debugShowCheckedModeBanner: false,
-        home: Center(
-          child: Container(
-            color: Colors.green,
-            alignment: Alignment.center,
-            constraints: const BoxConstraints(
-              maxHeight: 300.0,
-              maxWidth: 300.0,
-              minWidth: 300.0,
-              minHeight: 300.0,
-            ),
-            child: (Text(
-              'asdfsdfsdfdasdfsdfsdfsdasdfsdfsdfsd',
-            )),
+        home: Scaffold(
+          body: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  ctn(400, 50, 0xff6633FF),
+                  ctn(100, 100, 0xff33ffff),
+                  ctn(150, 150, 0xffcc0000),
+                  ctn(200, 200, 0xffccff00),
+                  ctn(250, 250, 0xff3366cc),
+                  ctn(300, 300, 0xff003300),
+                  ctn(350, 350, 0xff00FF99),
+                  ctn(400, 400, 0xff99CC00),
+                ],
+              ),
+              Container(
+                width: double.infinity,
+                height: 5,
+                color: const Color(0xffff0000),
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    color: const Color(0xff33ffff),
+                    constraints: const BoxConstraints(
+                      minWidth: 300,
+                      minHeight: 300,
+                      maxWidth: 400,
+                      maxHeight: 400,
+                    ),
+                  ),
+                  Container(
+                    color: const Color(0xffcc6666),
+                    width: 200,
+                    height: 200,
+                    constraints: const BoxConstraints(
+                      minWidth: 300,
+                      minHeight: 300,
+                      maxWidth: 400,
+                      maxHeight: 400,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+            /* child: Center(
+              child: Container(
+                color: Colors.green,
+                alignment: Alignment.center,
+                constraints: const BoxConstraints(
+                  maxHeight: 300.0,
+                  maxWidth: 300.0,
+                  minWidth: 300.0,
+                  minHeight: 300.0,
+                ),
+                child: (Text(
+                  'asdfsdfssdfsd',
+                )),
+              ),
+            ), */
           ),
-        )
-        /* Scaffold(
-        body: Container(
-          constraints: BoxConstraints(
-            maxHeight: 700,
-            maxWidth: 700,
-          ),
-          color: Colors.amber,
-          // height: 500,
-          // width: 500,
-          child: Container(
-            color: Colors.blue,
-            width: 300,
-            height: 300,
-          ),
-        ),
-      ), */
-        );
+        ));
   }
 
-/* 
-  @override
-  Widget build(BuildContext context) {
-    final List<String> entries = <String>['A', 'B', 'C'];
-    final List<int> colorCodes = <int>[600, 500, 100];
-
-    Widget listView = ListView(
-      padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        Text('listView'),
-        for (var i = 0; i < 2000; i++) item('listView'),
-      ],
-    );
-    final List<String> color = <String>['red', 'blue', 'green'];
-    Widget listViewBuilder = ListView.builder(
-      padding: const EdgeInsets.all(8),
-      itemCount: 6000,
-      itemBuilder: (BuildContext context, int index) {
-        return Container(
-          decoration: const BoxDecoration(
-            color: Color.fromARGB(255, 23, 20, 204),
-            // color: Colors.'${color}',
-            border: Border(
-              bottom: BorderSide(
-                color: Colors.white,
-                width: 5,
-                style: BorderStyle.solid,
-              ),
-            ),
+  Widget ctn(double x, double y, int rgb) {
+    return Container(
+      width: x,
+      height: y,
+      color: Color(rgb),
+      child: Center(
+          child: FittedBox(
+        child: Text(
+          'width: $x',
+          style: const TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.w700,
+            color: Color(0xffffffff),
           ),
-          height: 50,
-          child: const Center(
-            child: Text(
-              'listViewBuilder',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w700,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        );
-      },
-    );
-    Widget singleChildScrollView = SingleChildScrollView(
-      padding: const EdgeInsets.all(8),
-      child: Column(
-        children: [
-          Text('singleChildScrollView'),
-          for (var i = 0; i < 5000; i++) item('singleChildScrollView'),
-        ],
-      ),
-    );
-
-    return MaterialApp(
-      // debugShowCheckedModeBanner: false,
-      title: 'f-mon',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('ListView vs ListView.builder vs SingleChildScrollView+Column'),
-          backgroundColor: const Color.fromARGB(255, 17, 180, 80),
-          centerTitle: true,
-          // elevation: 0,
         ),
-        body: listView,
-        // body: listViewBuilder,
-        // body: singleChildScrollView,
-      ),
-    ); 
+      )),
+      // 0xff887766
+    );
   }
-*/
 
+  Widget ctnForConstraints(double x, double y, double minX, double minY, double maxX, double maxY, int rgb) {
+    return Container(
+      width: x,
+      height: y,
+      constraints: BoxConstraints(
+        minWidth: minX,
+        minHeight: minY,
+        maxWidth: maxX,
+        maxHeight: maxY,
+      ),
+      color: Color(rgb),
+      child: Center(
+          child: FittedBox(
+        child: Text(
+          'width: $x',
+          style: const TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.w700,
+            color: Color(0xffffffff),
+          ),
+        ),
+      )),
+      // 0xff887766
+    );
+  }
 }
