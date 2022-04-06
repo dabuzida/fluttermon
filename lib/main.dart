bc1025705ccd47c95bb3cd0b003ea2354fa62446
dabuzida <dabuzida@gmail.com>
2022-04-06 18:12:24 +0900

    test container

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ void showBaselines() {
 }
 
 void main() {
-  // showLayoutGuidelines();
+  showLayoutGuidelines();
 
   // showBaselines();
   return runApp(MyApp());
@@ -26,22 +26,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Constraint Sample',
-        // debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          body: Column(
+      title: 'Constraint Sample',
+      // debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
             children: <Widget>[
               Row(
                 children: <Widget>[
-                  ctn(400, 50, 0xff6633FF),
-                  ctn(100, 100, 0xff33ffff),
-                  ctn(150, 150, 0xffcc0000),
-                  ctn(200, 200, 0xffccff00),
-                  ctn(250, 250, 0xff3366cc),
-                  ctn(300, 300, 0xff003300),
-                  ctn(350, 350, 0xff00FF99),
-                  ctn(400, 400, 0xff99CC00),
+                  ctn(50, 50, 0xff33ffff),
+                  ctn(50, 50, 0xffcc0000),
+                  ctn(50, 50, 0xffccff00),
+                  ctn(50, 50, 0xff3366cc),
+                  ctn(50, 50, 0xff003300),
+                  ctn(50, 50, 0xff00FF99),
+                  ctn(50, 50, 0xffFFCC00),
+                  ctn(50, 50, 0xff9966FF),
+                  ctn(50, 50, 0xff663300),
+                  ctn(50, 50, 0xff669999),
+                  ctn(50, 50, 0xff00FF99),
+                  ctn(50, 50, 0xff99CC00),
+                  ctn(50, 50, 0xff33ffff),
+                  ctn(50, 50, 0xffcc0000),
+                  ctn(50, 50, 0xffccff00),
+                  ctn(50, 50, 0xff3366cc),
+                  ctn(50, 50, 0xff003300),
+                  ctn(50, 50, 0xff00FF99),
+                  ctn(50, 50, 0xffFFCC00),
+                  ctn(50, 50, 0xff9966FF),
+                  ctn(50, 50, 0xff663300),
+                  ctn(50, 50, 0xff669999),
+                  ctn(50, 50, 0xff00FF99),
+                  ctn(50, 50, 0xff99CC00),
+                  ctn(50, 50, 0xff33ffff),
+                  ctn(50, 50, 0xffcc0000),
+                  ctn(50, 50, 0xffccff00),
+                  ctn(50, 50, 0xff3366cc),
+                  ctn(50, 50, 0xff003300),
+                  ctn(50, 50, 0xff00FF99),
+                  ctn(50, 50, 0xffFFCC00),
+                  ctn(50, 50, 0xff9966FF),
+                  ctn(50, 50, 0xff663300),
+                  ctn(50, 50, 0xff669999),
+                  ctn(50, 50, 0xff00FF99),
                 ],
+              ),
+              Container(
+                width: double.infinity,
+                height: 5,
+                color: const Color(0xffff0000),
+              ),
+              Row(
+                children: <Widget>[],
               ),
               Container(
                 width: double.infinity,
@@ -51,16 +87,15 @@ class MyApp extends StatelessWidget {
               Row(
                 children: <Widget>[
                   Container(
-                    color: const Color(0xff33ffff),
                     constraints: const BoxConstraints(
                       minWidth: 300,
                       minHeight: 300,
                       maxWidth: 400,
                       maxHeight: 400,
                     ),
+                    color: const Color(0xff000000),
                   ),
                   Container(
-                    color: const Color(0xffcc6666),
                     width: 200,
                     height: 200,
                     constraints: const BoxConstraints(
@@ -69,27 +104,78 @@ class MyApp extends StatelessWidget {
                       maxWidth: 400,
                       maxHeight: 400,
                     ),
+                    color: const Color(0xff6699FF),
+                  ),
+                  Container(
+                    width: 350,
+                    height: 350,
+                    constraints: const BoxConstraints(
+                      minWidth: 300,
+                      minHeight: 300,
+                      maxWidth: 400,
+                      maxHeight: 400,
+                    ),
+                    color: const Color(0xffee1155),
+                    child: Container(
+                      width: 200,
+                      height: 200,
+                      color: Color(0xff778833),
+                    ),
+                  ),
+                  Container(
+                    // width: 300,
+                    // height: 300,
+                    constraints: const BoxConstraints(
+                      minWidth: 300,
+                      minHeight: 300,
+                      // maxWidth: 400,
+                      // maxHeight: 400,
+                    ),
+                    color: const Color(0xff99bbcc),
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      color: Color(0xff44dd99),
+                    ),
+                  ),
+                  //
+
+                  Container(
+                    constraints: const BoxConstraints(
+                      minWidth: 300,
+                      minHeight: 300,
+                      maxWidth: 400,
+                      maxHeight: 400,
+                    ),
+                    color: const Color(0xff786858),
+                    child: Container(
+                      color: const Color(0xffffff00),
+                      width: 2250,
+                      height: 2250,
+                    ),
                   ),
                 ],
               ),
             ],
             /* child: Center(
-              child: Container(
-                color: Colors.green,
-                alignment: Alignment.center,
-                constraints: const BoxConstraints(
-                  maxHeight: 300.0,
-                  maxWidth: 300.0,
-                  minWidth: 300.0,
-                  minHeight: 300.0,
+                child: Container(
+                  color: Colors.green,
+                  alignment: Alignment.center,
+                  constraints: const BoxConstraints(
+                    maxHeight: 300.0,
+                    maxWidth: 300.0,
+                    minWidth: 300.0,
+                    minHeight: 300.0,
+                  ),
+                  child: (Text(
+                    'asdfsdfssdfsd',
+                  )),
                 ),
-                child: (Text(
-                  'asdfsdfssdfsd',
-                )),
-              ),
-            ), */
+              ), */
           ),
-        ));
+        ),
+      ),
+    );
   }
 
   Widget ctn(double x, double y, int rgb) {
@@ -98,43 +184,56 @@ class MyApp extends StatelessWidget {
       height: y,
       color: Color(rgb),
       child: Center(
-          child: FittedBox(
-        child: Text(
-          'width: $x',
-          style: const TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.w700,
-            color: Color(0xffffffff),
+        child: FittedBox(
+          child: Text(
+            'width: $x',
+            style: const TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.w700,
+              color: Color(0xffffffff), //0xff887766
+            ),
           ),
         ),
-      )),
-      // 0xff887766
+      ),
     );
   }
 
-  Widget ctnForConstraints(double x, double y, double minX, double minY, double maxX, double maxY, int rgb) {
+  Widget ctn2(
+    double p,
+    double q,
+    /* double minX,
+    double minY,
+    double maxX,
+    double maxY, */
+    // int rgb,
+    double r,
+    double s,
+  ) {
     return Container(
-      width: x,
-      height: y,
-      constraints: BoxConstraints(
-        minWidth: minX,
-        minHeight: minY,
-        maxWidth: maxX,
-        maxHeight: maxY,
+      width: p,
+      height: q,
+      constraints: const BoxConstraints(
+        minWidth: 300,
+        minHeight: 300,
+        maxWidth: 400,
+        maxHeight: 400,
       ),
-      color: Color(rgb),
-      child: Center(
-          child: FittedBox(
-        child: Text(
-          'width: $x',
-          style: const TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.w700,
-            color: Color(0xffffffff),
+      color: const Color(0xff000000),
+      child: Container(
+        width: r,
+        height: s,
+        color: const Color(0xff9966FF),
+        child: FittedBox(
+          child: Text(
+            'width: $p',
+            style: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w700,
+              color: Color(0xff00ff66),
+            ),
           ),
         ),
-      )),
-      // 0xff887766
+      ),
     );
   }
 }
