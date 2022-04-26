dadad70c78981541e3b22e20820966e5f73149c2
dabuzida <dabuzida@gmail.com>
2022-04-26 17:54:55 +0900

    test DateTime

--- lib/test.dart
+++ lib/test.dart
@@ -1,5 +1,4 @@
 import 'dart:collection';
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -12,20 +11,34 @@ class Test extends StatefulWidget {
 
 class _TestState extends State<Test> {
   final DateTime now = DateTime.now();
-  final DateFormat formatter = DateFormat('yyyy-MM-dd');
-  DoubleLinkedQueue x = DoubleLinkedQueue();
-  DateTime.parse(formattedString)
+  final DateFormat format = DateFormat('yyyy-MM-dd');
+  final DateFormat format2 = DateFormat('yyyy년 MM월 dd일 hh시 mm분 ss초');
+  String get formatted => format.format(now);
+  String get formatted2 => format2.format(now);
+  // final String formatted = format.format(now);
+  // print(formatted);
+
+  _TestState() {
+    // final String formatted2 = format2.format(now);
+    // print(formatted);
+    // print(formatted2);
+  }
+
+  // DateTime.parse(formattedString)
   Person p1 = Person(name: 'dd', image: 232, timeCreated: DateTime.now().toString());
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(children: [
-        Text(
-          p1.name,
-        ),
-        Text(p1.image.toString()),
-        Text(p1.timeCreated),
-      ]),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(p1.name),
+          Text(p1.image.toString()),
+          Text(p1.timeCreated),
+          Text(formatted),
+          Text(formatted2),
+        ],
+      ),
     );
   }
 }
